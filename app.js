5f636396678e1d9da92d5812fe9d5ac4112e1081
Ian Schneider <ian@composedcreative.com>
2024-03-04 12:35:44 -0500

    added api caching to localStorage

--- app.js
+++ app.js
@@ -8,41 +8,49 @@
 
             this.boundListeners = []
 
+
             this.init()
         }
        
         async init(){
-            let data = await this.data.getAllPokemon()
-            if( data ){
+
+            this.data.checkStorage()
+
+            let allPokemonRequest = await this.data.getAllPokemon()
+            if( allPokemonRequest ){
                 this.setFinderText()
                 this.enableButtons()
                 this.setAllPokemon()
                 this.enableCarousel()
             }else{
                 console.log("no pokemon")
-            }   
+            }
         }
        
         setFinderText(){
-            this.view.characterSelect.randomButton.innerText = this.text.randomButton
+            // this.view.characterSelect.randomButton.innerText = this.text.randomButton
             // this.view.characterSelect.updateText( this.text.intro, this.view.characterSelect.prompt )
             // this.view.characterSelect.updateText( this.text.select, this.view.characterSelect.select )
         }
         
         setAllPokemon(){
-            for(let pokemon of this.data.allPokemon){
 
-                let option = document.createElement( 'option' )
-                option.value = pokemon.name
-                option.textContent = pokemon.name.replaceAll( "-", " " )
+            this.data.allPokemon.sort( ( a, b ) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0 )
+            
+            // for(let pokemon of this.data.allPokemon){
 
-                this.view.characterSelect.select.append( option )
-            }
+            //     let option = document.createElement( 'option' )
+            //     option.value = pokemon.name
+            //     option.textContent = pokemon.name.replaceAll( "-", " " )
+
+            //     this.view.characterSelect.select.append( option )
+            // }
+         
+
         }
        
-        async setPokemon(pokemon){
-            
-            let data = await this.data.getPokemon( pokemon )
+        async setPokemon(pokemon){ 
+            data = await this.data.getPokemon( pokemon )
             if( data ){
 
                 this.view.characterSelect.stats.abilities.innerHTML = ""
@@ -84,37 +92,36 @@
                 // this.view.characterSelect.abilities. = pokemon.replaceAll("-"," ")
             }else{
                 console.log("whoopsies something went wrong - try to catch them all later")
-            }   
+            }
         }
         
         enableButtons(){
-            this.view.characterSelect.randomButton.addEventListener( 'click', this.randomButtonClickHandler )
-
-            this.view.characterSelect.select.addEventListener( 'change', this.selectChangeHandler )
+            // this.view.characterSelect.randomButton.addEventListener( 'click', this.randomButtonClickHandler )
+            // this.view.characterSelect.select.addEventListener( 'change', this.selectChangeHandler )
         }
 
         enableCarousel(){
-            this.view.characterSelect.carousel.nextBtn.addEventListener('click', event => {
-                if( this.data.currentPokemonSprites.length ){
-                    if( this.data.currentPokemonSpriteIndex < this.data.currentPokemonSprites.length - 1 ){
-                        this.data.currentPokemonSpriteIndex ++ 
-                    }else{
-                        this.data.currentPokemonSpriteIndex = 0
-                    }
-                    this.view.characterSelect.carousel.images.src =  this.data.currentPokemonSprites[this.data.currentPokemonSpriteIndex]
-                }
-            })
+            // this.view.characterSelect.carousel.nextBtn.addEventListener('click', event => {
+            //     if( this.data.currentPokemonSprites.length ){
+            //         if( this.data.currentPokemonSpriteIndex < this.data.currentPokemonSprites.length - 1 ){
+            //             this.data.currentPokemonSpriteIndex ++ 
+            //         }else{
+            //             this.data.currentPokemonSpriteIndex = 0
+            //         }
+            //         this.view.characterSelect.carousel.images.src =  this.data.currentPokemonSprites[this.data.currentPokemonSpriteIndex]
+            //     }
+            // })
 
-            this.view.characterSelect.carousel.prevBtn.addEventListener('click', event => {
-                if( this.data.currentPokemonSprites.length ){
-                    if( this.data.currentPokemonSpriteIndex > 0 ){
-                        this.data.currentPokemonSpriteIndex -- 
-                    }else{
-                        this.data.currentPokemonSpriteIndex = this.data.currentPokemonSprites.length - 1
-                    }
-                    this.view.characterSelect.carousel.images.src =  this.data.currentPokemonSprites[this.data.currentPokemonSpriteIndex]
-                }
-            })
+            // this.view.characterSelect.carousel.prevBtn.addEventListener('click', event => {
+            //     if( this.data.currentPokemonSprites.length ){
+            //         if( this.data.currentPokemonSpriteIndex > 0 ){
+            //             this.data.currentPokemonSpriteIndex -- 
+            //         }else{
+            //             this.data.currentPokemonSpriteIndex = this.data.currentPokemonSprites.length - 1
+            //         }
+            //         this.view.characterSelect.carousel.images.src =  this.data.currentPokemonSprites[this.data.currentPokemonSpriteIndex]
+            //     }
+            // })
         }
 
         randomButtonClickHandler = ( event ) => {
@@ -134,29 +141,89 @@
     window.onload = () => {
 
         // data object that holds the current pokemon and functions for fetching pokemon
-        const data = {
+        window.pokedata = {
             currentPokemon: "",
             currentPokemonSprites: [],
             currentPokemonSpriteIndex: 0,
             allPokemon: [],
+            allPokemonMoves: [],
+            storedPokemon: [],
+            cpuHasPlayedAs: [],
             baseUrl: "https://pokeapi.co/api/v2/",
+            hasLocalStorage: false,
+            
             async getAllPokemon(){
 
-                const response = await this.getData("pokemon?limit=100000&offset=0")
+                if( this.hasLocalStorage ){
+                    
+                    let allCharacters = this.getLocalStorageItem('all_characters')
+
+                    console.log("allCharacters", allCharacters)
+                   
+                    if( !allCharacters ){
+    
+                        console.log("does NOT have all characters, but create LS")
+    
+                        response = await this.getData( "generation/1/" )
+    
+                        if( response.sucess ){
+                            this.setLocalStorage( "all_characters", response.data.pokemon_species)
+                            this.setLocalStorage( "all_moves", response.data.moves)
+                            
+                            this.setLocalStorage( "stored_pokemon", JSON.stringify([]) )
+                            this.setLocalStorage( "cpu_used_characters", JSON.stringify([]) )
+                        }
+
+                    }else{
+                        console.log('DOES have all characters', allCharacters)
+
+                        this.allPokemon = this.getLocalStorageItem('all_characters')
+                        this.allPokemonMoves = this.getLocalStorageItem('all_moves')
+                        this.storedPokemon = this.getLocalStorageItem('stored_pokemon')
+                        this.cpuHasPlayedAs = this.getLocalStorageItem('cpu_used_characters')
+
+                        return true
+                    }
+                }else{
+                    console.log('does NOT have all characters or LS')
+                    response = await this.getData( "generation/1/" )
+                }
+
                 if(response.sucess){
-                    this.allPokemon = response.data.results
-                    this.allPokemon.sort( ( a, b ) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0 )
+                    this.allPokemon = response.data.pokemon_species
+                    console.log(response.data.pokemon_species)
+                    this.allPokemonMoves = response.data.moves
                     return true
                 }else{
                     return false
-                }
-
-                
+                }                
             },
             async getPokemon(pokemon){
                 //fetch pokemon
-                const response = await this.getData("pokemon/" + pokemon)
-                if(response.sucess){
+                if( this.data.hasLocalStorage ){
+                    const stroredCharacters = this.data.getLocalStorageItem('stored_pokemon')
+                    let response = null
+                    if( !stroredCharacters.length ){
+                        response = await this.getData( "pokemon/" + pokemon )
+                        if( response.sucess ){
+                            this.storedPokemon.push( response.data )
+                            this.data.setLocalStorageItem('stored_pokemon',response.data )
+                        }
+                    }else{
+                        for (let character in this.storedPokemon){
+                            if(character.name === pokemon){
+
+                                this.currentPokemon = character
+                                
+                                return  this.currentPokemon
+                            }
+                        }
+                    }
+                }else{
+                    response = await this.getData( "pokemon/" + pokemon )
+                }
+                // const response = await this.getData( "pokemon/" + pokemon )
+                if( response.sucess ){
                     this.currentPokemon = response.data
                     return this.currentPokemon
                 }else{
@@ -164,18 +231,12 @@
                 }
             },
             formatPokemonName(pokemon){
-                return pokemon.replaceAll(" ", "-")
+                return pokemon.replaceAll( " ", "-" )
             },
             reformatPokemonName(pokemon){
-                pokemon = pokemon.replaceAll("-", " ")
+                pokemon = pokemon.replaceAll( "-", " " )
                 const name = this.toTitleCase( pokemon )
                 return name
-            },
-            //https://www.w3docs.com/snippets/javascript/how-to-convert-string-to-title-case-with-javascript.html
-            toTitleCase(str) {
-                return str.toLowerCase().split(' ').map(function (word) {
-                  return (word.charAt(0).toUpperCase() + word.slice(1));
-                }).join(' ');
             },
             getRandomPokemon(){
                 return this.allPokemon [ Math.floor( Math.random() * this.allPokemon.length ) ]
@@ -193,13 +254,53 @@
                     }
                 )
                 return r
+            },
+            //https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+            storageAvailable () {
+                let storage;
+                try {
+                    storage = window['localStorage'];
+                    const x = '__storage_test__';
+                    storage.setItem(x, x);
+                    storage.removeItem(x);
+                    return true;
+                }
+                catch (e) {
+                    return e instanceof DOMException && (
+                        // everything except Firefox
+                        e.code === 22 ||
+                        // Firefox
+                        e.code === 1014 ||
+                        // test name field too, because code might not be present
+                        // everything except Firefox
+                        e.name === 'QuotaExceededError' ||
+                        // Firefox
+                        e.name === 'NS_ERROR_DOM_QUOTA_REACHED' ) &&
+                        // acknowledge QuotaExceededError only if there's something already stored
+                        (storage && storage.length !== 0);
+                }
+            },
+            checkStorage(){
+                console.log("checkStorage")
+                if( this.storageAvailable() ){ 
+                   this.hasLocalStorage = true
+                }
+            },
+            getLocalStorageItem(item){
+               console.log( "item",JSON.parse( localStorage.getItem( `__ils_poke_battle__${item}` ) ),  `__ils_poke_battle__${item}` )
+               return JSON.parse( localStorage.getItem( `__ils_poke_battle__${item}` ) )
+            },
+            setLocalStorage(item, value){
+                localStorage.setItem(`__ils_poke_battle__${item}`, JSON.stringify( value ) )
             }
+            
         }
 
         // view object for storing dom elements
         const views = {
             main: document.querySelector('.main-view'),
             intro:{
+                view: document.querySelector('.intro-view'),
                 vsCPUBtn: document.querySelector('#vsCPUBtn'),
                 vsPlayerBtn: document.querySelector('#vsPlayerBtn'),
                 gameInstructionsBtn: document.querySelector('#gameInstructionsBtn'),
@@ -209,33 +310,54 @@
                 }
             },
             characterSelect:{
-                select: document.querySelector('#pokemon-select'),
-                randomButton: document.querySelector('.random-pokemon-button'),
-                textPrompt:  document.querySelector('.prompt'),
-                carousel: {
-                    prevBtn: document.querySelector('#prev-btn'),
-                    nextBtn: document.querySelector('#next-btn'),
-                    images:  document.querySelector('.images img'),
+                view: document.querySelector('.character-select-view'),
+                player1:{
+                    select: document.querySelector('.character-select-view').querySelector('.player-1').querySelector('.pokemon-select'),
+                    randomButton: document.querySelector('.character-select-view').querySelector('.player-1').querySelector('.random-pokemon-button'),
+                    carousel: {
+                        prevBtn: document.querySelector('.character-select-view').querySelector('.player-1').querySelector('#prev-btn'),
+                        nextBtn: document.querySelector('.character-select-view').querySelector('.player-1').querySelector('#next-btn'),
+                        images:  document.querySelector('.character-select-view').querySelector('.player-1').querySelector('.images img'),
+                    },
+                    stats: {
+                        name: document.querySelector('.character-select-view').querySelector('.player-1').querySelector('.name'),
+                    },
                 },
-                stats: {
-                    name: document.querySelector('.name'),
-                    abilities: document.querySelector('.abilities'),
-                    moves: document.querySelector('.moves'),
+                player2:{
+                    select: document.querySelector('.character-select-view').querySelector('.player-2').querySelector('.pokemon-select'),
+                    randomButton: document.querySelector('.character-select-view').querySelector('.player-2').querySelector('.random-pokemon-button'),
+                    carousel: {
+                        prevBtn: document.querySelector('.character-select-view').querySelector('.player-2').querySelector('#prev-btn'),
+                        nextBtn: document.querySelector('.character-select-view').querySelector('.player-2').querySelector('#next-btn'),
+                        images:  document.querySelector('.character-select-view').querySelector('.player-2').querySelector('.images img'),
+                    },
+                    stats: {
+                        name: document.querySelector('.character-select-view').querySelector('.player-2').querySelector('.name'),
+                    },
                 },
-                
+                cpu:{
+                    carousel: {
+                        prevBtn: document.querySelector('.character-select-view').querySelector('.cpu').querySelector('#prev-btn'),
+                        nextBtn: document.querySelector('.character-select-view').querySelector('.cpu').querySelector('#next-btn'),
+                        images:  document.querySelector('.character-select-view').querySelector('.cpu').querySelector('.images img'),
+                    },
+                    stats: {
+                        name: document.querySelector('.character-select-view').querySelector('.cpu').querySelector('.name'),
+                    },
+                }
             },
             updatePrompt: ( text ) => {
                 // console.log("updatePrompt:",text,  views.textPrompt)
                 // views.textPrompt.textContent = text
+            },
+             //https://www.w3docs.com/snippets/javascript/how-to-convert-string-to-title-case-with-javascript.html
+             toTitleCase(str) {
+                return str.toLowerCase().split(' ').map(function (word) {
+                  return (word.charAt(0).toUpperCase() + word.slice(1));
+                }).join(' ');
             }
         }
 
-        const finderText = {
-            intro: "welcome",
-            randomButton: "random pokémon", 
-            select: 'pick a pokémon'
-        }
-
-        const poke = new PikAPokemon(data, views, finderText)
+        const poke = new PikAPokemon( window.pokedata, views, {} )
 
     }
